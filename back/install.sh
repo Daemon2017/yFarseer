c231dc24e67a4bf4cb3d691a8012de57dc691456
Daemon2017 <daemon2017@gmail.com>
2026-08-31 22:40:08 +0300

    Правка сборки

--- back/install.sh
+++ back/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 apt-get update
-python3 -m pip install -r requirements.txt
+python3 -m pip install --no-cache-dir torch==2.8.0 --index-url https://download.pytorch.org/whl/cpu
+python3 -m pip install --no-cache-dir -r requirements.txt
